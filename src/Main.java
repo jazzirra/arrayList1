eb6c34582b7db4b437e019df2d612e2896149187
1 <itstadejaziraasankadyrov@gmail.com>
2023-03-22 14:15:42 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,185 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         //ARRAY LIST
+
+//        ArrayList<String>fff=new ArrayList<>(); //1
+//        fff.add("HI ");
+//        fff.add("my name is Kageyama. ");
+//        fff.add("how are you? ");
+//        String a = fff.get(1);
+//        fff.set(1, fff.get(2));
+//        fff.set(2, a);
+//        System.out.println(fff);
+
+//        ArrayList<String>fff2 = new ArrayList<>(); //2
+//        fff2.add("Black");
+//        fff2.add("Blue");
+//        fff2.add("Braun");
+//        fff2.add("Pink");
+//        fff2.add("White");
+//        fff2.add("Red");
+//        fff2.remove(5);
+//        System.out.println(fff2);
+
+//        ArrayList<String>fff3=new ArrayList<>(); //3
+//        fff3.add("j");
+//        fff3.add("e");
+//        fff3.add("a");
+//        fff3.add("d");
+//        fff3.add("c");
+//        fff3.add("f");
+//        fff3.add("b");
+//        Collections.sort(fff3);
+//        System.out.println(fff3);
+//
+//        ArrayList<Integer>f=new ArrayList<>(); //3
+//        f.add(543);
+//        f.add(3);
+//        f.add(43);
+//        f.add(0);
+//        f.add(-3);
+//        Collections.sort(f);
+//        System.out.println(f);
+
+//        ArrayList<String>fsov=new ArrayList<>(); //4
+//        ArrayList<String>ff4= new ArrayList<>();
+//        ff4.add("jazira");
+//        ff4.add("acema");
+//        ff4.add("nurjan");
+//        ff4.add("adil");
+//        ArrayList<String>fff4= new ArrayList<>();
+//        fff4.add("jazira");
+//        fff4.add("sezim");
+//        fff4.add("nurkyz");
+//        fff4.add("adil");
+//        for (int i = 0; i < ff4.size(); i++) {
+//            if (ff4.get(i).contains(fff4.get(i))){
+//              System.out.println(fff4.get(i));
+//              fsov.add(fff4.get(i));
+//          }
+//          else{
+//              System.out.println("элементы не совподают");
+//          }
+//        }
+//      System.out.println("совподающие элементы:"+fsov);
+
+
+
+//        ArrayList<String>ff5= new ArrayList<>(); //5
+//        ff5.add("jazira");
+//        ff5.add("acema");
+//        ff5.add("nurjan");
+//        ArrayList<String>fff5= new ArrayList<>();
+//        fff5.add("sezim");
+//        fff5.add("nurkyz");
+//        fff5.add("adil");
+//        for (int i = 0; i < fff5.size(); i++) {
+//            ff5.add(fff5.get(i));
+//        }
+//        System.out.println(ff5);
+
+//        Scanner scanner= new Scanner(System.in);//6
+//        String scan = scanner.nextLine();
+//        ArrayList<String>fff6=new ArrayList<>();
+//        fff6.add("sezim");
+//        fff6.add("nurkyz");
+//        fff6.add("adil");
+//        fff6.add("asema");
+//        fff6.add("nurjan");
+//        int aa= 0;
+//        for (int i = 0; i < fff6.size() ;i++ ) {
+//            if (fff6.get(i).contains(scan)) {
+//                System.out.println("элемент найден : " + fff6.get(i));
+//            }else {
+//                aa++;
+//                if (aa== fff6.size()){
+//                    System.out.println("элемент не найден");
+//                }
+//            }
+//        }
+
+//        ArrayList<String>fff7=new ArrayList<>();//7
+//        ArrayList<String>ff7=new ArrayList<>();
+//        fff7.add("guchi");
+//        fff7.add("nike");
+//        fff7.add("puma");
+//        fff7.add("d&b");
+//        fff7.add("adidas");
+//        int tt=0;
+//        for (int i = 0; i < fff7.size(); i++) {
+//            tt++;
+//            ff7.add(fff7.get(i));
+//        if (tt== fff7.size()) {
+//            fff7.clear(); }
+//    }
+//        System.out.println(ff7);
+//        System.out.println(fff7);
+
+         //LINKED LIST
+//       LinkedList<String>fff=new LinkedList<>(); //1
+//        fff.add("boy");
+//        fff.add("girl");
+//        fff.add("baby");
+//        String a = fff.get(0);
+//        fff.set(0, fff.get(1));
+//        fff.set(1, a);
+//        System.out.println(fff);
+
+
+//        ArrayList<String>arrayList2=new ArrayList<>();//2
+//        arrayList2.add("asema");
+//        arrayList2.add("ayana");
+//        arrayList2.add("sezim");
+//        arrayList2.add("nurjan");
+//        arrayList2.add("kamila");
+//        arrayList2.set(0, arrayList2.get(2));
+//        System.out.println(arrayList2);
+
+
+//        ArrayList<String>arrayList3=new ArrayList<>();//3
+//        arrayList3.add("asema");
+//        arrayList3.add("ayana");
+//        arrayList3.add("sezim");
+//        arrayList3.add("nurjan");
+//        arrayList3.add("kamila");
+//        arrayList3.remove(0);
+//        int kl=0;
+//        for (int i = 0; i < arrayList3.size(); i++) {
+//            kl++;
+//            if (kl== arrayList3.size()){
+//                arrayList3.remove(i);
+//            }
+//        }
+//        System.out.println(arrayList3);
+
+
+//        ArrayList<String>arrayList4=new ArrayList<>();  //4
+//        ArrayList<String>arrayList=new ArrayList<>();
+//        arrayList4.add("asema");
+//        arrayList4.add("ayana");
+//        arrayList4.add("sezim");
+//        arrayList4.add("nurjan");
+//        arrayList4.add("kamila");
+//        int t = 0;
+//        for (int i = 0; i < arrayList4.size(); i++) {
+//            t++;
+//            arrayList.add(arrayList4.get(i));
+//        if (t== arrayList4.size()) {
+//            arrayList4.clear(); }
+//    }
+//        System.out.println(arrayList);
+//        System.out.println(arrayList4);
+
+
+        
+
+
+
+
+
+
+    }
+
+}
